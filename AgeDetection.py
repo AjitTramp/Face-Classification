9e665163b5b56469bb5215740a9a4c6206928b79
AjitTramp <35846758+AjitTramp@users.noreply.github.com>
2018-06-06 12:38:55 +0530

    Update AgeDetection.py

--- AgeDetection.py
+++ AgeDetection.py
@@ -1,24 +1,13 @@
+data_dir='/home/ajit/Desktop/AgeDetection'
+
+% pylab inline
 import os
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
-dirName= "/media/ajit/5E64-0F5C/Humpback"
-from subprocess import check_output
-print(check_output(["ls",dirName]).decode("utf8"))
+import random
 
-os.chdir(dirName)
+import pandas as pd
+from scipy.misc import imread
 
-# Get the list of training files 
-train = os.listdir(dirName+'/train')
-# Get the list of test files
-test = os.listdir(dirName+'/test')
+root_dir = os.path.abspath('.')
 
-print("Total number of training images: ",len(train))
-print("Toal number of test images: ",len(test))
-
-
-
-sample = pd.read_csv(dirName+'/sample_submission.csv')
-print(sample.shape)
-sample.head()
+train = pd.read_csv(os.path.join(data_dir, 'train.csv'))
+test = pd.read_csv(os.path.join(data_dir, 'test.csv'))
